392267ca46852d1baa5279b1eafdf0aee730ec75
Mikhail Korobkin <korobkin66@gmail.com>
2023-11-10 03:24:12 +0100

    push no1

--- backend/foodgram/recipes/management/commands/check.py
+++ backend/foodgram/recipes/management/commands/check.py
@@ -1,27 +1,19 @@
 from django.core.management.base import BaseCommand
-from recipes.models import Shoppingcart
+from recipes.models import Ingredient
+import json
 # import os
 # from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'Описание вашей команды'
+    help = 'Заполнение ингридиентов'
 
     def handle(self, *args, **options):
-        json = {}
-        shopping_recipe = Shoppingcart.objects.filter(user=4).all()
-        for recipe in shopping_recipe:
+        with open('data/ingredients.json', 'r') as f:
+            data = json.load(f)
 
-            quantity = recipe.recipe.quan_recipe.all()
-
-            for a in quantity:
-                print(a.ingredient.name, a.amount, a.ingredient.unit)
-                dict_amount = 0
-                if a.ingredient.name in json:
-                    dict_amount = json[a.ingredient.name]['Количество']
-
-                json[a.ingredient.name] = {'Количество':
-                                           a.amount + dict_amount,
-                                           'Ед.изм.': a.ingredient.unit}
-                print(json)
+        for i in data:
+            igrd = Ingredient.objects.create(name=i['name'],
+                                             measurement_unit=i['measurement_unit'])
+            igrd.save()
         self.stdout.write(self.style.SUCCESS('Привет, это ваша команда!'))
