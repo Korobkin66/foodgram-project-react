4163b5bd0dc3759e431fb15d97444ff3411de4bc
Mikhail Korobkin <korobkin66@gmail.com>
2023-11-19 01:14:43 +0100

    dc update

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,19 +1,32 @@
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from .views import UserViewSet
 
-app_name = 'users'
-
 router = DefaultRouter()
-
-router.register('users', UserViewSet, basename='users')
-
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('auth/', include("djoser.urls.authtoken")),
-    path('users/subscriptions/',
-         UserViewSet.as_view({'get': 'subscriptions'}), name='subscriptions'),
-    path('', include('djoser.urls')),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('api/users/subscriptions/',
+         UserViewSet.as_view({'get': 'subscriptions'}),
+         name='user-subscriptions'),
+         
 ]
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+
+# from .views import UserViewSet
+
+# app_name = 'users'
+
+# router = DefaultRouter()
+
+# router.register('users', UserViewSet, basename='users')
+
+
+# urlpatterns = [
+#     path('auth/', include("djoser.urls.authtoken")),
+#     path('api/', include(router.urls)),
+#     path('', include('djoser.urls')),
+#     path('', include(router.urls)),
+# ]
