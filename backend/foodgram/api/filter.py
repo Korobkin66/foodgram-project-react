768444ec9d0cd174d70c7ab030de6a6b61154e08
Korobkin <korobkin66@gmail.com>
2024-01-16 18:36:24 +0000

    tags

--- backend/foodgram/api/filter.py
+++ backend/foodgram/api/filter.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django_filters import FilterSet, filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
@@ -10,6 +10,11 @@ class RecipeFilter(FilterSet):
     )
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
+    )
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
     )
 
     class Meta:
