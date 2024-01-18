0482f90256f2d17707cd75bd6e6a9a573703a80c
Korobkin <korobkin66@gmail.com>
2024-01-18 21:58:20 +0000

    pag def rem

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -5,7 +5,7 @@ class MyPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
 
-    def paginate_queryset(self, queryset, request, view=None):
-       if queryset is not None:
-           queryset = queryset.order_by('id')
-       return super().paginate_queryset(queryset, request, view)
+    # def paginate_queryset(self, queryset, request, view=None):
+    #    if queryset is not None:
+    #        queryset = queryset.order_by('id')
+    #    return super().paginate_queryset(queryset, request, view)
