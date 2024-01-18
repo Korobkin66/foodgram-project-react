aff54220aace5018896dd4e9f2da26b99fc2acb0
Korobkin <korobkin66@gmail.com>
2024-01-18 18:51:04 +0000

    to_representation

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -134,11 +134,9 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'cooking_time')
         model = Recipe
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['ingredients'] = MiniRecipesSerializer(
-            instance.ingredients.all(), many=True).data
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     return MiniRecipesSerializer(instance, many=True).data
 
     def get_is_favorited(self, obj):
         current_user = self.context['request'].user
