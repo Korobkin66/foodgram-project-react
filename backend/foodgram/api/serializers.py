f8057ffd34cccc85ae3c65c0956a49dcb0101c78
Korobkin <korobkin66@gmail.com>
2024-01-19 01:52:41 +0000

    654321

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -164,7 +164,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 
             ingredients = []
             for ingredient_data in ingredients_data:
-                ingredient_id = ingredient_data.get('id')
+                # ingredient_id = ingredient_data.get('id')
+                ingredient_id = ingredient_data.get('ingredient', {}).get('id') # attempt
                 amount = ingredient_data.get('amount')
                 ingredient = Ingredient.objects.get(id=ingredient_id)
                 ingredients.append(Quantity(recipe=instance,
