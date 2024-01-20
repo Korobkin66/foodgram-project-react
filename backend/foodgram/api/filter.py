67680d011ae34c05b1eb3cff02541a9435ea8f05
Korobkin <korobkin66@gmail.com>
2024-01-20 20:55:44 +0000

    what if?

--- backend/foodgram/api/filter.py
+++ backend/foodgram/api/filter.py
@@ -95,12 +95,12 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated:
-            return queryset.filter(shopping_list__user=self.request.user)
+            return queryset.filter(cart__user=self.request.user)
         return queryset
 
     class Meta:
