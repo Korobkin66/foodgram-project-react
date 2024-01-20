df46e8a7eb180cf7c4da0254c0a595bb7c20e2c4
Korobkin <korobkin66@gmail.com>
2024-01-20 20:09:06 +0000

    self del from get_is_favorited

--- backend/foodgram/api/filter.py
+++ backend/foodgram/api/filter.py
@@ -52,15 +52,15 @@ class RecipeFilter(FilterSet):
     def get_is_favorited(self, queryset, name, value):
         logger.info('queryset_fav', queryset)
         logger.info('value_fav', value)
-        if self.request.user.is_authenticated and value == 1:
-            logger.info('self.request.user', self.request.user)
-            return queryset.filter(favorites__user=self.request.user)
+        if request.user.is_authenticated and value == 1:
+            logger.info('request.user', request.user)
+            return queryset.filter(favorites__user=request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         logger.info('value', value)
-        if self.request.user.is_authenticated and value == 1:
-            return queryset.filter(cart__user=self.request.user)
+        if request.user.is_authenticated and value == 1:
+            return queryset.filter(cart__user=request.user)
         return queryset
 
 
