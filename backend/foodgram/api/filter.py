1d0d551241f98c0607e186b47078c0e4f13feb12
Korobkin <korobkin66@gmail.com>
2024-01-20 21:37:37 +0000

    B2

--- backend/foodgram/api/filter.py
+++ backend/foodgram/api/filter.py
@@ -14,13 +14,13 @@ class IngredientFilter(filters.FilterSet):
 class RecipeFilter(filters.FilterSet):
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart',
-    )
-    # tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+        method='get_is_in_shopping_cart')
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all(),)
+        queryset=Tag.objects.all())
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all())
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated:
