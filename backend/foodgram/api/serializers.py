f2f8f1e4668b9924a1c35ad256abe67909738df2
Korobkin <korobkin66@gmail.com>
2024-01-22 02:47:54 +0000

    2 again

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -79,13 +79,21 @@ class FollowSerializer(BaseUserSerializer):
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj).count()
 
+    # def get_recipes(self, obj):
+    #     request = self.context.get('request')
+    #     limit = request.GET.get('recipes_limit')
+    #     queryset = Recipe.objects.filter(author=obj)
+    #     if limit:
+    #         queryset = queryset[:int(limit)]
+    #     return MiniRecipesSerializer(queryset, many=True).data
+
     def get_recipes(self, obj):
-        request = self.context.get('request')
-        limit = request.GET.get('recipes_limit')
         queryset = Recipe.objects.filter(author=obj)
-        if limit:
-            queryset = queryset[:int(limit)]
-        return MiniRecipesSerializer(queryset, many=True).data
+        if recipes_limit := self.context['request'].GET.get(
+                'recipes_limit'):
+            queryset = queryset[:int(recipes_limit)]
+        return MiniRecipesSerializer(queryset,
+                                     many=True).data
 
 
 class SubscribeSerializer(serializers.ModelSerializer):
