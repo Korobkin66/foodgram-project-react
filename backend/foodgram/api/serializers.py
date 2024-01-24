50167e6de0ef88e03cac13d13aa5810ab53f5b01
Korobkin <korobkin66@gmail.com>
2024-01-24 01:45:33 +0000

    l

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -90,11 +90,11 @@ class FollowSerializer(BaseUserSerializer):
 
 
 class SubscribeSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='following.id')
+    # id = serializers.ReadOnlyField(source='following.id')
     # email = serializers.ReadOnlyField(source='following.email')
     # username = serializers.ReadOnlyField(source='following.username')
     # first_name = serializers.ReadOnlyField(source='following.first_name')
-    last_name = serializers.ReadOnlyField(source='following.last_name')
+    # last_name = serializers.ReadOnlyField(source='following.last_name')
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
